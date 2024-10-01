51a99c707baa3543758e9fadf8bdd3bd03266ad9
Mohd Norhaimi bin Yahya <haimiyahya@gmail.com>
2024-10-01 14:11:06 +0800

    increment the ua_isomm version instead, previously wrongly updated the ThousandIsland version.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule UaIsomm.MixProject do
   def project do
     [
       app: :ua_isomm,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -22,7 +22,7 @@ defmodule UaIsomm.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:thousand_island, "~> 1.3.2"}
+      {:thousand_island, "~> 1.3"}
     ]
   end
 end
